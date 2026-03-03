1bad9015fd9640dae1ae90764d7ed0a44d45cbf4
Nun <suon_phanun@sr.bbu.edu.kh>
2026-03-03 15:40:47 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'M SCMS',
+      title: 'SCMS',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: kPrimaryColor),
